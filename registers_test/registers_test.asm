da67f910ca5769e1c7ed4592a5dd82d2b8bb4374
itabarr <itabarr825@gmail.com>
2023-01-16 11:15:54 +0200

    add registers test

---
+++ registers_test/registers_test.asm
@@ -0,0 +1,16 @@
+# This test checks use with all registers
+add $v0, $zero, $imm, 2
+add $a0, $zero, $imm, 3
+add $a1, $zero, $imm, 4
+add $a2, $zero, $imm, 5
+add $a3, $zero, $imm, 6
+add $t0, $zero, $imm, 7
+add $t1, $zero, $imm, 8
+add $t2, $zero, $imm, 9
+add $s0, $zero, $imm, 10
+add $s1, $zero, $imm, 11
+add $s2, $zero, $imm, 12
+add $gp, $zero, $imm, 13
+add $sp, $zero, $imm, 14
+add $ra, $zero, $imm, 15
+halt $zero, $zero, $zero, 0 
